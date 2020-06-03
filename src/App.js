58fcd0dd252c168f0cc0c812bbece27ae0290799
Chris Morrow <cmorrow181@gmail.com>
2020-06-03 18:30:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a className="navbar-brand">Brand</a>
+          <button className="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div id="my-nav" className="collapse navbar-collapse">
+          </div>
+        </nav>
       </header>
+      <p>
+        <button type="button" class="btn btn-primary">Primary button!</button>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col col-6">
+              left
+            </div>
+            <div className="col col-6">
+              right
+            </div>
+          </div>
+        </div>
+      </p>
     </div>
   );
 }
